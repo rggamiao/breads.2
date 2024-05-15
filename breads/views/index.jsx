8b86824f9a08b1d4886af32e76dd745593a4234a
rggamiao <rejicamiling@gmail.com>
2024-05-15 13:52:48 -0700

    added models

--- breads/views/index.jsx
+++ breads/views/index.jsx
@@ -1,11 +1,31 @@
 const React = require('react')
-const Default = require('./layouts/Default')
+const Default = require('./layouts/Default.jsx')
+const breads = require('../controllers/breads_controller.js')
+const { title } = require('process')
 
-function Index () {
+function Index ({breads, title})  {
     return (
-      <Default>
+        <Default title = {title}>
         <h2>Index Page</h2>
+        <ul>
+        <div className="newButton">
+            <a href="/breads/new"><button>Add a new bread</button></a>
+        </div>
+        {
+            breads.map((bread, index)=> {
+                return (
+                <li key={index}>
+                    <a href={`/breads/${bread.id}`}>
+    {bread.name}
+</a>
+
+                </li>
+                )
+            })
+        }
+        </ul>
       </Default>
+      
     )
 }
 
--- server.js
+++ server.js
@@ -1,36 +1,34 @@
-// DEPENDENCIES
+// Dependencies
 const express = require('express')
 
-// CONFIGURATION
+// Config
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
-// MIDDLEWARE
+// Middleware
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-// MIDDLEWARE
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
 
-
-// ROUTES
+// Routes
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
-  
-// Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
-  
 
-// LISTEN
-app.listen(PORT, () => {
-  console.log('listening on port', PORT);
+// Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
+// 404 Page - Always at the end of the route order
+app.get('*', (req, res) => {
+  res.send('error404')
 })
 
-// 404 Page
-app.get('*', (req, res) => {
-  res.send('404')
+// Listener
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
 })
